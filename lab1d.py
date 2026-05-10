785facee79819b9f184fa6cefc705c34bdca09c2
Chao Zhang <nilsforever1981@gmail.com>
2026-05-10 13:50:35 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Description: This program will print out "10 + 2 * 5 = 20" (the printout 
+should change if the values in the objects change)'''
+
+# Any line that starts with a "#" is also known as a comment,
+# these lines are ignored by the python interpreter even if
+# they contain code. The very first line is called a Shebang line, 
+# it is used to tell the system which interpreter to 
+# use(python2, python3, bash, etc).
+
+num1=10
+num2=2
+num3=5
+sum=num1+num2*num3
+print("10 + 2 * 5 = "+str(sum))
